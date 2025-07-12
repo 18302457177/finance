9b7767405c73b20907c604053322e1b28baca1c5
xxs <x3304845146@gmail.com>
2025-07-12 14:05:22 +0800

    异常类

---
+++ song-finance-common/src/main/java/com/song/common/exception/BizException.java
@@ -0,0 +1,24 @@
+package com.song.common.exception;
+
+
+import com.song.common.constant.ApiResponseCode;
+
+/**
+ * 业务处理异常
+ */
+public class BizException extends BaseException {
+
+    private static final long serialVersionUID = 628904681759624791L;
+
+    public BizException(String message) {
+        super(ApiResponseCode.BUSINESS_ERROR.getCode(), message);
+    }
+
+    public BizException(int code, String message) {
+        super(code, message);
+    }
+
+    public BizException(String message, Throwable t) {
+        super(message, t);
+    }
+}
---
+++ song-finance-common/src/main/java/com/song/common/exception/LoginException.java
@@ -0,0 +1,20 @@
+package com.song.common.exception;
+
+import com.song.common.constant.ApiResponseCode;
+
+/**
+ * 登录异常
+ */
+public class LoginException extends BaseException {
+
+    private static final long serialVersionUID = 979094253305695687L;
+
+    public LoginException(String message) {
+        super(ApiResponseCode.LOGIN_ERROR.getCode(), message);
+    }
+
+    public LoginException(String message, Throwable t) {
+        super(message, t);
+    }
+}
+
